3789051aa50887c0f7a15b0087fa7f052b92c1eb
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-05-29 18:32:58 -0400

    finished assignment

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -1,4 +1,4 @@
-var customerName = "Alliance  ";
+let customerName = "Alliance  ";
 
 // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
 function addCustomerName() {
@@ -8,17 +8,28 @@ function addCustomerName() {
     
     // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
 
+    let header = document.getElementById('tm-header')
+    header.textContent = customerName
     // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
 
+    let introHeader = document.getElementById('introheader')
+    introHeader.textContent = customerName
 };
 
 function toggleColors() {
     // <!-- todo: Change all classes named 'tm-white-rect' to 'tm-blue-rect' -->
 
+    let el = document.querySelectorAll('.tm-white-rect')
+    for(let i = 0;i < el.length;i++){
+        el[i].classList.add('tm-blue-rect')
+        el[i].classList.remove('tm-white-rect')
+    }
 };
 
 function hideElement() {
     //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
+    let socialLink = document.querySelector('.tm-social-links')
+    socialLink.style.visibility = 'none'
 
 };
 
@@ -33,6 +44,20 @@ function hideElement() {
     //<p id="aboutpara2"> <!--todo: Add customer company motto text-->
 
     // <p id="addresspara" class="mb-3"> <!--todo: Add customer company phone number text-->
+    let intro = document.getElementById('intropora1')
+    intro.textContent = 'Alliance was founded in 1931 by Albert Daniels.'
+
+    let intro2 = document.getElementById('intropara2')
+    intro2.textContent ='The company is a leader in P.C. Manufacturing'
+
+    let aboutPara1 = getElementById('aboutpara1')
+    aboutPara1.textContent ='We deliver the best service this side of creation' 
+
+    let aboutPara2 = document.getElementById('aboutpara2')
+    aboutPara2.textContent ='Our motto is satisfaction guaranteed' 
+
+    let addresspara = document.getElementById('addresspara')
+    addresspara.textContent = '1-800-565-5656'
 
 }; 
 
